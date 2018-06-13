e121fb3bab8e41aa1716b2deade6608f7f29b7a2
prmoreira23 <prm.gredes@gmail.com>
2018-06-13 09:40:52 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,9 @@
+require 'pry'
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :destroy, :update]
+
   def index
+    @artists = Artist.all
   end
 
   def show
@@ -11,7 +15,6 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.new(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -20,12 +23,9 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
 
     if @artist.save
@@ -36,7 +36,6 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
@@ -46,5 +45,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count_text
+    "#{self.song_count} #{'song'.pluralize(self.song_count)}"
+  end
+
+  def song_count
+    songs.count
+  end
 end
